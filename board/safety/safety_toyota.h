00b3ea4908c86c8935f6fda53d205d78843fb8d2
【TIM】 <90225659+CT921@users.noreply.github.com>
2024-02-20 20:48:20 +0800

    Update safety_toyota.h

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -45,7 +45,7 @@ const int TOYOTA_GAS_INTERCEPTOR_THRSLD = 805;
 
 // Stock longitudinal
 #define TOYOTA_COMMON_TX_MSGS                                                                                     \
-  {0x2E4, 0, 5}, {0x191, 0, 8}, {0x412, 0, 8}, {0x343, 0, 8}, {0x1D2, 0, 8}, {0x750, 0, 8}, /* white list 0x750 for Enhanced Diagnostic Request */ /* LKAS + LTA + ACC & PCM cancel cmds */  \
+  {0x2E4, 0, 5}, {0x191, 0, 8}, {0x412, 0, 8}, {0x343, 0, 8}, {0x1D2, 0, 8},  /* LKAS + LTA + ACC & PCM cancel cmds */  \
 
 #define TOYOTA_COMMON_LONG_TX_MSGS                                                                                                          \
   TOYOTA_COMMON_TX_MSGS                                                                                                                     \
@@ -358,9 +358,9 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
   // UDS: Only tester present ("\x0F\x02\x3E\x00\x00\x00\x00\x00") allowed on diagnostics address
   if (addr == 0x750) {
     // this address is sub-addressed. only allow tester present to radar (0xF)
-    bool invalid_uds_msg = (GET_BYTES(to_send, 0, 4) != 0x003E020FU) || (GET_BYTES(to_send, 4, 4) != 0x0U) || (GET_BYTES(to_send, 0, 4) != 0x60100241U) || (GET_BYTES(to_send, 0, 4) != 0x01100241U) || (GET_BYTES(to_send, 0, 4) != 0x60100242U)|| (GET_BYTES(to_send, 0, 4) != 0x01100242U) || (GET_BYTES(to_send, 0, 4) != 0x69210241U) || (GET_BYTES(to_send, 0, 4) != 0x69210242U);
-    if (invalid_uds_msg) {
-      tx = false;
+    bool invalid_uds_msg = (GET_BYTES(to_send, 0, 4) != 0x003E020FU) || (GET_BYTES(to_send, 4, 4) != 0x0U);
+    if (false && invalid_uds_msg) {
+      tx = 0;
     }
   }
 
