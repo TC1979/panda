2bb3fe16bd1c8f47aeb25a06fc576a2a925bc436
CT921 <tim.chen242@gmail.com>
2024-02-21 13:58:21 +0800

    Update safety_toyota.h

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -45,7 +45,7 @@ const int TOYOTA_GAS_INTERCEPTOR_THRSLD = 805;
 
 // Stock longitudinal
 #define TOYOTA_COMMON_TX_MSGS                                                                                     \
-  {0x2E4, 0, 5}, {0x191, 0, 8}, {0x412, 0, 8}, {0x343, 0, 8}, {0x1D2, 0, 8},  /* LKAS + LTA + ACC & PCM cancel cmds */  \
+  {0x2E4, 0, 5}, {0x191, 0, 8}, {0x412, 0, 8}, {0x343, 0, 8}, {0x1D2, 0, 8}, {0x750, 0, 8}, /* dp - white list 0x750 for Enhanced Diagnostic Request */ /* LKAS + LTA + ACC & PCM cancel cmds */  \
 
 #define TOYOTA_COMMON_LONG_TX_MSGS                                                                                                          \
   TOYOTA_COMMON_TX_MSGS                                                                                                                     \
@@ -358,9 +358,15 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
   // UDS: Only tester present ("\x0F\x02\x3E\x00\x00\x00\x00\x00") allowed on diagnostics address
   if (addr == 0x750) {
     // this address is sub-addressed. only allow tester present to radar (0xF)
-    bool invalid_uds_msg = (GET_BYTES(to_send, 0, 4) != 0x003E020FU) || (GET_BYTES(to_send, 4, 4) != 0x0U) || (GET_BYTES(to_send, 0, 4) != 0x60100241U) || (GET_BYTES(to_send, 0, 4) != 0x01100241U) || (GET_BYTES(to_send, 0, 4) != 0x60100242U)|| (GET_BYTES(to_send, 0, 4) != 0x01100242U) || (GET_BYTES(to_send, 0, 4) != 0x69210241U) || (GET_BYTES(to_send, 0, 4) != 0x69210242U);
-    if (false && invalid_uds_msg) {
-      tx = 0;
+    bool invalid_uds_msg = (GET_BYTES(to_send, 0, 4) != 0x003E020FU) || (GET_BYTES(to_send, 4, 4) != 0x0U);
+
+    // DP: Secret sauce.
+    bool dp_valid_uds_msgs = (GET_BYTES(to_send, 0, 4) == 0x10002141) || (GET_BYTES(to_send, 0, 4) == 0x60100241) || (GET_BYTES(to_send, 0, 4) == 0x69210241);
+    dp_valid_uds_msgs |= (GET_BYTES(to_send, 0, 4) == 0x10002142) || (GET_BYTES(to_send, 0, 4) == 0x60100242) || (GET_BYTES(to_send, 0, 4) == 0x10002142) || (GET_BYTES(to_send, 0, 4) == 0x69210242);
+    dp_valid_uds_msgs |= (GET_BYTES(to_send, 0, 4) == 0x11300540);
+
+    if (invalid_uds_msg && !dp_valid_uds_msgs) {
+      tx = false;
     }
   }
 
