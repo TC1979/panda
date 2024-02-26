94fca15c8ab5a8e4d095f85b7b9e5387ceff0e7a
CT921 <tim.chen242@gmail.com>
2024-02-26 14:36:03 +0800

    Update safety_toyota.h

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -243,7 +243,7 @@ static void toyota_rx_hook(const CANPacket_t *to_push) {
 }
 
 static bool toyota_tx_hook(const CANPacket_t *to_send) {
-  sport_mode = alternative_experience & ALT_EXP_RAISE_LONGITUDINAL_LIMITS_TO_ISO_MAX;
+  sport_mode = alternative_experience && ALT_EXP_RAISE_LONGITUDINAL_LIMITS_TO_ISO_MAX;
 
   bool tx = true;
   int addr = GET_ADDR(to_send);
